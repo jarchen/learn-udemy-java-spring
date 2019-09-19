be00c9d5c3860e1e915bb978dd65eeecc5304583
Jargo Chen <jargotw@gmail.com>
2019-09-19 14:45:38 +0800

    Autowired Prototype with proxyMode

---
+++ spring-in-5-steps/src/main/java/my/learn/spring/basic/springin5steps/SpringIn5StepsScopeApplication.java
@@ -0,0 +1,26 @@
+package my.learn.spring.basic.springin5steps;
+
+import my.learn.spring.basic.springin5steps.scope.PersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringIn5StepsScopeApplication {
+    private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsScopeApplication.class, args);
+
+        PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
+        PersonDAO personDAO2 = applicationContext.getBean(PersonDAO.class);
+
+        LOGGER.info("{}", personDAO);
+        LOGGER.info("{}", personDAO.getJdbcConnection());
+
+        LOGGER.info("{}", personDAO2);
+        LOGGER.info("{}", personDAO2.getJdbcConnection());
+    }
+}
