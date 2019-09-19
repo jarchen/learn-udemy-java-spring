8db5d50b454e5707d4e01efca7133377639ba607
Jargo Chen <jargotw@gmail.com>
2019-09-19 17:48:20 +0800

    ComponentScan in ApplicationContext

--- spring-in-5-steps/src/main/java/my/learn/spring/basic/springin5steps/SpringIn5StepsXMLApplication.java
+++ spring-in-5-steps/src/main/java/my/learn/spring/basic/springin5steps/SpringIn5StepsXMLApplication.java
@@ -1,19 +1,21 @@
 package my.learn.spring.basic.springin5steps;
 
 import my.learn.spring.basic.springin5steps.xml.XmlPersonDAO;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class SpringIn5StepsXMLApplication {
+    private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsXMLApplication.class);
 
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+
+            LOGGER.info("Beans Loaded -> {}", (Object) applicationContext.getBeanDefinitionNames());
+
             XmlPersonDAO personDAO = applicationContext.getBean(XmlPersonDAO.class);
-            System.out.println(personDAO);
-            System.out.println(personDAO.getXmlJdbcConnection());
+
+            LOGGER.info("{}, {}", personDAO, personDAO.getXmlJdbcConnection());
         }
     }
 }
