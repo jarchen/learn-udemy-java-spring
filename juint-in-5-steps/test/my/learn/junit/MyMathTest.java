ea1dfdddbf6b2148bd67b83b2974782c0e772302
Jargo Chen <jargotw@gmail.com>
2019-09-20 17:42:05 +0800

    Test Before and After

--- juint-in-5-steps/test/my/learn/junit/MyMathTest.java
+++ juint-in-5-steps/test/my/learn/junit/MyMathTest.java
@@ -1,19 +1,41 @@
 package my.learn.junit;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class MyMathTest {
     private MyMath myMath = new MyMath();
 
+    @Before
+    public void before() {
+        System.out.println("Before");
+    }
+
+    @After
+    public void after() {
+        System.out.println("After");
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("BeforeClass");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("AfterClass");
+    }
+
     @Test
     public void sumWith3Numbers() {
+        System.out.println("Test1");
         assertEquals(6, myMath.sum(new int[]{1, 2, 3}));
     }
 
     @Test
     public void sumWith1Numbers() {
+        System.out.println("Test2");
         assertEquals(3, myMath.sum(new int[]{3}));
     }
 }
