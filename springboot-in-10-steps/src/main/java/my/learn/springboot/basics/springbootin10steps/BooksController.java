16d03e253c2c08f98ebf9b656b5b5326a38aad67
Jargo Chen <jargotw@gmail.com>
2019-09-25 16:44:12 +0800

    Update Spring Boot DevTools

--- springboot-in-10-steps/src/main/java/my/learn/springboot/basics/springbootin10steps/BooksController.java
+++ springboot-in-10-steps/src/main/java/my/learn/springboot/basics/springbootin10steps/BooksController.java
@@ -11,7 +11,7 @@ public class BooksController {
     @GetMapping("/books")
     public List<Book> getAllBooks() {
         return Arrays.asList(
-                new Book(1L, "Mastering Spring 5.0", "Ranga Karnam")
+                new Book(1L, "Mastering Spring 5.1", "Ranga Karnam")
         );
     }
 }
