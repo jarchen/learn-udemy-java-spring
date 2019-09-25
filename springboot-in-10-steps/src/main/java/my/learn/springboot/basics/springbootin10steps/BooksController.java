e825aec5ca06bc599467fcc689ecf140ed83b299
Jargo Chen <jargotw@gmail.com>
2019-09-25 16:47:01 +0800

    Little modify for Spring Boot with DevTools

--- springboot-in-10-steps/src/main/java/my/learn/springboot/basics/springbootin10steps/BooksController.java
+++ springboot-in-10-steps/src/main/java/my/learn/springboot/basics/springbootin10steps/BooksController.java
@@ -4,13 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 public class BooksController {
     @GetMapping("/books")
     public List<Book> getAllBooks() {
-        return Arrays.asList(
+        return Collections.singletonList(
                 new Book(1L, "Mastering Spring 5.1", "Ranga Karnam")
         );
     }
