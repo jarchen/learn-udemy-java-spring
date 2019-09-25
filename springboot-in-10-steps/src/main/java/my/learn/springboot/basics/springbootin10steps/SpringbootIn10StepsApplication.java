27bff553556471354f9755d54e8bf4fbedb27f73
Jargo Chen <jargotw@gmail.com>
2019-09-25 15:21:36 +0800

    Spring Boot Auto Configuration

--- springboot-in-10-steps/src/main/java/my/learn/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ springboot-in-10-steps/src/main/java/my/learn/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,12 +2,16 @@ package my.learn.springboot.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+        for (String name : applicationContext.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
     }
 
 }
