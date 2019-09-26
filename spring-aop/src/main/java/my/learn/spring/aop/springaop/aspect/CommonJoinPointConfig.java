4afea8844974530641870e317eda6b8f24e676f1
Jargo Chen <>
2019-09-26 11:56:22 +0800

    Update Spring AOP with others PointCut

--- spring-aop/src/main/java/my/learn/spring/aop/springaop/aspect/CommonJoinPointConfig.java
+++ spring-aop/src/main/java/my/learn/spring/aop/springaop/aspect/CommonJoinPointConfig.java
@@ -4,8 +4,22 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class CommonJoinPointConfig {
     @Pointcut("execution(* my.learn.spring.aop.springaop.data.*.*(..))")
-    public void dataLayerExecution() {}
+    public void dataLayerExecution() {
+    }
 
     @Pointcut("execution(* my.learn.spring.aop.springaop.business.*.*(..))")
-    public void businessLayerExecution() {}
+    public void businessLayerExecution() {
+    }
+
+    @Pointcut("my.learn.spring.aop.springaop.aspect.CommonJoinPointConfig.dataLayerExecution() && my.learn.spring.aop.springaop.aspect.CommonJoinPointConfig.businessLayerExecution()")
+    public void allLayerExecution() {
+    }
+
+    @Pointcut("bean(*dao*)")
+    public void beanContainingDao() {
+    }
+
+    @Pointcut("within(my.learn.spring.aop.springaop.data..*)")
+    public void dataLayerExecutionWithWithin() {
+    }
 }
