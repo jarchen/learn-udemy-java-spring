47c82a3a16fba9566a7278025c7ca9a1d5e206ea
Jargo Chen <>
2019-09-26 16:16:59 +0800

    Create JDBC DAO

---
+++ database-demo/src/main/java/my/learn/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,19 @@
+package my.learn.database.databasedemo.jdbc;
+
+import my.learn.database.databasedemo.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDao {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from PERSON", new BeanPropertyRowMapper<>(Person.class));
+    }
+}
