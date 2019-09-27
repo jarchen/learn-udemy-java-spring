36e69c2b96c178064953547bf5635aec42692b7a
Jargo Chen <>
2019-09-27 17:00:21 +0800

    Update Spring JPA with deleteById

--- database-demo/src/main/java/my/learn/database/databasedemo/JpaDemoApplication.java
+++ database-demo/src/main/java/my/learn/database/databasedemo/JpaDemoApplication.java
@@ -27,6 +27,7 @@ public class JpaDemoApplication implements CommandLineRunner {
         logger.info("User id 10001 -> {}", repository.findById(10001));
         logger.info("Inserting -> {}", repository.insert(new Person("Tara", "Berlin", new Date())));
         logger.info("Updating 10003 -> {}", repository.update(new Person(10003, "Pieter", "Utrecht", new Date())));
+        repository.deleteById(10002);
 
         /*
         logger.info("All users -> {}", repository.findAll());
