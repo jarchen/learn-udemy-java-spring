9fc3dc21c1048fd49a966b12ba696af4cf5a2286
Jargo Chen <>
2019-09-27 15:26:48 +0800

    Update Spring JDBC with Custom RowMapper

--- database-demo/src/main/java/my/learn/database/databasedemo/jdbc/PersonJdbcDao.java
+++ database-demo/src/main/java/my/learn/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -4,8 +4,11 @@ import my.learn.database.databasedemo.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -14,12 +17,29 @@ public class PersonJdbcDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    class PersonRowMapper implements RowMapper<Person> {
+        @Override
+        public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+            Person person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setLocation(resultSet.getString("location"));
+            person.setBirthDate(resultSet.getTimestamp("birth_date"));
+
+            return person;
+        }
+    }
+
     public List<Person> findAll() {
-        return jdbcTemplate.query("select * from PERSON", new BeanPropertyRowMapper<Person>(Person.class));
+        return jdbcTemplate.query("select * from PERSON", new PersonRowMapper());
     }
 
     public Person findById(int id) {
-        return jdbcTemplate.queryForObject("select * from PERSON where id=?", new Object[]{id}, new BeanPropertyRowMapper<Person>(Person.class));
+        return jdbcTemplate.queryForObject(
+                "select * from PERSON where id=?",
+                new Object[]{id},
+                new BeanPropertyRowMapper<Person>(Person.class)
+        );
     }
 
     public int deleteById(int id) {
@@ -27,12 +47,22 @@ public class PersonJdbcDao {
     }
 
     public int insert(Person person) {
-        return jdbcTemplate.update("insert into person (id, name, location, birth_date) values (?, ?, ?, ?)",
-                person.getId(), person.getName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime()));
+        return jdbcTemplate.update(
+                "insert into person (id, name, location, birth_date) values (?, ?, ?, ?)",
+                person.getId(),
+                person.getName(),
+                person.getLocation(),
+                new Timestamp(person.getBirthDate().getTime())
+        );
     }
 
     public int update(Person person) {
-        return jdbcTemplate.update("update person set name=?, location=?, birth_date=? where id=?",
-                person.getName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime()), person.getId());
+        return jdbcTemplate.update(
+                "update person set name=?, location=?, birth_date=? where id=?",
+                person.getName(),
+                person.getLocation(),
+                new Timestamp(person.getBirthDate().getTime()),
+                person.getId()
+        );
     }
 }
