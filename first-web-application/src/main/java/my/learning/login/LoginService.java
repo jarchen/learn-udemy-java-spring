54c381ea380a1eeefd259c4257fd57e8d38c5a6b
Jargo Chen <>
2019-10-01 17:32:20 +0800

    Update Spring MVC with Autowired and Injection

--- first-web-application/src/main/java/my/learning/login/LoginService.java
+++ first-web-application/src/main/java/my/learning/login/LoginService.java
@@ -1,5 +1,8 @@
 package my.learning.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
     public boolean validateUser(String user, String password) {
         return user.equalsIgnoreCase("abc") && password.equals("123");
